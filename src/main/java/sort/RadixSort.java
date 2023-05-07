372c046a799b3186964dfc6387f7aa39a29fdf58
vivek <vivek@coadjute.com>
2023-05-07 23:51:10 +0530

    radix sort

--- src/main/java/sort/RadixSort.java
+++ src/main/java/sort/RadixSort.java
@@ -1,8 +1,10 @@
 package sort;
 
 import java.util.Arrays;
+import java.util.List;
+
 /**
-radix sort : sorting with help of counting sort
+ * radix sort : sorting with help of counting sort
  * splitting integers into separate value according to base (preferably n)
  * will give us O(n) time if total distinct values k will be or order O(n^a)
  * i.e. polynomial in n
@@ -12,25 +14,48 @@ radix sort : sorting with help of counting sort
  */
 public class RadixSort {
 
-    private static int getTotalDigitsForBase(int digit , int b){
-        var pow = 0 ;
-        while(digit >= Math.pow(b, pow)) pow++;
+    private static int getTotalDigitsForBase(int digit, int b) {
+        var pow = 0;
+        while (digit >= Math.pow(b, pow)) pow++;
         return pow;
     }
 
-    private RadixSort(){}
+    private RadixSort() {
+    }
 
-    public static Integer[] sort(Integer [] keys){
+    public static Integer[] sort(Integer[] keys) {
         var n = Math.max(keys.length, 10);
-        if(keys.length == 0) return keys;
-        var digits = getTotalDigitsForBase(Arrays.stream(keys).max(Integer::compareTo).orElseGet(() -> keys[0]) , n);
-        var temp = Arrays.copyOf(keys , keys.length);
-        for(int i = 1 ; i <= digits ; i++){
+        if (keys.length == 0) return keys;
+        var digits = getTotalDigitsForBase(Arrays.stream(keys).max(Integer::compareTo).orElseGet(() -> keys[0]), n);
+        var temp = Arrays.copyOf(keys, keys.length);
+        for (int i = 0; i < digits; i++) {
             int finalI = i;
-            var countingSort = new CountingSort<Integer>(n , j -> (int) (((int)(j % Math.pow(n , finalI))) / (Math.pow(n , finalI-1.0))));
+            var countingSort = new CountingSort<Integer>(n, j -> getBitOfZAtPlaceXForBaseY(j, finalI, n));
             temp = countingSort.sort(temp);
-
         }
         return temp;
+    }
+
+    /**
+     * return the bit value of ZbaseY at place x.
+     * eg 89base4 = 1121
+     * so for args= (89,1,4) result will be 2 (89base4 value at position 1 )
+     *
+     * @param z no to get bits map of
+     * @param x place of bit (O indexed)
+     * @param y base
+     * @return bit value at place x
+     */
+    private static int getBitOfZAtPlaceXForBaseY(int z, int x, int y) {
+        return (int) (((int) (z % Math.pow(y, (x + 1)))) / (Math.pow(y, x)));
+    }
+
+
+    public static List<Integer> sort(List<Integer> keys) {
+
+        var keyAsArray = keys.toArray(new Integer[0]);
+        keyAsArray = sort(keyAsArray);
+        return Arrays.stream(keyAsArray).toList();
+
     }
 }
